27f24c5e0081a7c85dfb99437f1531fa51e4609f
Vlad Losev <vladimir.losev@sage.com>
2021-07-09 16:45:06 -0700

    Requires either key or token ih bootstrap script.

--- scripts/bootstrap-gitops.sh
+++ scripts/bootstrap-gitops.sh
@@ -8,7 +8,7 @@ function die {
 }
 
 function usage {
-  die "Usage: $0 -k <gitub-private-key-file> [-n <namespace] [-p <start-path>]"
+  die "Usage: $0 [-k <gitub-private-key-file>] [-n <namespace] [-p <start-path>]"
 }
 
 start_path=live/argo
@@ -25,32 +25,41 @@ while getopts p:n:k: OPTNAME ; do
 done
 shift $((OPTIND - 1))
 
-if test -z "$github_key_file" ; then
- die "GitHub private key file must be specified via -k"
+if test -z "$github_key_file" -a -z "$GITHUB_TOKEN" ; then
+ die " Either specify GitHub private key with -k or a GitHub token" \
+     "via the GITHUB_TOKEN environment variable.\n" \
+     "The key or token should be created with these scopes:\n" \
+     "public_repo, read:gpg_key, repo:status, repo_deployment"
 fi
-
-if test -z "$GITHUB_TOKEN" ; then
-  die "GITHUB_TOKEN environment variable must be set to a token with these scopes:\npublic_repo, read:gpg_key, repo:status, repo_deployment"
-fi
-
 
 if ! kubectl get namespace "$namespace" >/dev/null 2>&1 ; then
   kubectl create namespace "$namespace"
 fi
-github_key=$(cat "$github_key_file" | base64)
-host_key=$(ssh-keyscan -p 1975 storage.losev.com 2>/dev/null | base64)
-cat <<EOT | kubectl apply -f -
+{
+cat <<EOT
   apiVersion: v1
   kind: Secret
   metadata:
     namespace: $namespace
     name: flux-deploy-user-creds
+EOT
+if test -n "$github_key_file" ; then
+  github_key=$(cat "$github_key_file" | base64)
+  github_public_key=$(ssh-keygen -y -f "$github_key_file" | base64)
+  host_key=$(ssh-keyscan -p 1975 storage.losev.com 2>/dev/null | base64)
+  cat <<EOT
   data:
     identity: "$github_key"
     known_hosts: "$host_key"
+    identity.pub: "$github_public_key"
+EOT
+fi
+if test -n "$GITHUB_TOKEN" ; then
+  cat <<EOT
   stringData:
-    identity.pub: $(ssh-keygen -y -f "$github_key_file")
     username: git
     password: $GITHUB_TOKEN
 EOT
+fi
+} | kubectl apply -f -
 kustomize build "$start_path" | kubectl apply -f -
